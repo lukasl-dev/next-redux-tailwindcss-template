1cc58c254969b33b0d9b5ed9fe06a6b166f2364c
lukasl-dev <git@lukasl.dev>
2022-03-25 10:27:53 +0100

    fix(index page): eslint issues

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
-import type { NextPage } from 'next'
+import React from 'react'
+import { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return <></>
 }
 
+// noinspection JSUnusedGlobalSymbols
 export default Home
