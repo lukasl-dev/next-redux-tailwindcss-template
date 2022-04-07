929093f2d93c294c26adb31431c6f86886c6b674
lukasl-dev <git@lukasl.dev>
2022-04-07 20:06:58 +0200

    feat(app): integrate nprogress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,15 @@ import '@/styles/globals.css'
 import React from 'react'
 import { NextPage } from 'next'
 import { AppProps } from 'next/app'
+import { Router } from 'next/router'
+import nProgress from 'nprogress'
+
+nProgress.configure({ showSpinner: false })
+Router.events.on('routeChangeStart', () => nProgress.start())
 
 const App: NextPage<AppProps> = ({ Component, pageProps }) => {
+  nProgress.done()
+
   return <Component {...pageProps} />
 }
 
